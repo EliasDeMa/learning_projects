f1536d4cd57bd5a70c90bcd78f0023e5582352c6
Demare <elias.demaertelaere@gmail.com>
2018-08-29 14:04:15 +0200

    third program

---
+++ topdown_triangle/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    let mut input = String::new();
+
+    print!("Input a number: ");
+    io::stdout().flush().expect("Unable to flush stdout");
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let num: u64 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please input a valid number");
+            std::process::exit(1);
+        }
+    };
+
+    print_stars(&num);
+}
+
+fn print_stars(rows: &u64) {
+    // ..= for an inclusive range
+    for i in (1..=*rows).rev() {
+        // This setup lets us print a string multiple times
+        print!("{:*<1$}", "", i as usize);
+        println!("");
+    }
+}
